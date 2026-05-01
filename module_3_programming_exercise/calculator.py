593741a5839df598214ae254fff1c39727979685
rainieljerez <rainiel.jerez@gmail.com>
2026-05-01 11:02:49 +0800

    defined return functions to return a float value

--- module_3_programming_exercise/calculator.py
+++ module_3_programming_exercise/calculator.py
@@ -1,19 +1,33 @@
-#i plan to use tkinter as the ui of the calculator
+#Use tkinter as the ui of the calculator
+
 import tkinter as tk
 from tkinter import messagebox
 
-#calculator math functions
+#Calculator math functions
+def add(num_1, num_2):
+    return num_1 + num_2
 
-def add(a, b):
-    return a + b
+def subtract(num_1, num_2):
+    return num_1 - num_2
 
-def subtract(a, b):
-    return a - b
+def multiply(num_1, num_2):
+    return num_1 * num_2
 
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
+def divide(num_1, num_2):
+    if num_2 == 0:
         raise ValueError ("Cannot divide by zero")
-    return a / b
+    return num_1 / num_2
+
+def calculate(operations, num_1, num_2):
+    ops = {
+        "Addition": (add, "+"),
+        "Subtraction": (subtract, "-"),
+        "Multiplication": (multiply, "*"),
+        "Division": (divide, "/")
+    }
+    func, symbol =ops[operations]
+    return symbol, func(num_1, num_2)
+#Returns a float value
+def format_result(value):
+    return f"{float(value):.10g}"
+
